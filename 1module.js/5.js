8fdb1de1350a598994170f3b3f91549ffedb8e1e
Evgeniya Bayda <baje75128@gmail.com>
2021-05-25 13:29:24 +0300

    5

---
+++ 1module.js/5.js
@@ -0,0 +1,6 @@
+const productName = "Droid";
+const pricePerItem = 3500;
+
+// Change code below this line
+const message = "You picked Droid, price per item is 3500 credits";
+console.log(message);

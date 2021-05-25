734592d3ed9551938280b419a37a656f7e1c0047
Evgeniya Bayda <baje75128@gmail.com>
2021-05-25 13:02:46 +0300

    2

---
+++ 1module.js/2.js
@@ -0,0 +1,9 @@
+let productName = "Droid";
+productName = "Repair droid";
+console.log(productName);
+
+let pricePerItem = 2000;
+pricePerItem = 3500;
+console.log(pricePerItem);
+
+// Change code below this line

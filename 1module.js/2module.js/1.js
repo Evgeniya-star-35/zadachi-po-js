ce58b508632a539e46a6afdfd9b12e04d2c74dc3
Evgeniya Bayda <baje75128@gmail.com>
2021-05-28 19:03:15 +0300

    commit 2 module

---
+++ 1module.js/2module.js/1.js
@@ -0,0 +1,70 @@
+  //xxxxxxxxxxxxxxxxxxxxxзадача № 1 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+
+//   function checkAge(age) {
+//   if (age >= 18) { // Change this line
+//     return "You are an adult";
+//   }
+
+//   return "You are a minor";
+// }
+ //xxxxxxxxxxxxxxxxxxxxxзадача № 2 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+
+
+//  function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   // Change code below this line
+
+ 
+
+//   if (password === ADMIN_PASSWORD) {
+   
+  
+//   return "Welcome!";
+   
+//   }
+
+//   return "Access denied, wrong password!";
+//   // Change code above this line
+// }
+
+
+//xxxxxxxxxxxxxxxxxxxxxзадача № 3 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+
+
+// function checkStorage(available, ordered) {
+//   // Change code below this line
+ 
+
+//   if (ordered === 0) {
+    
+//    return "Your order is empty!";
+    
+//   } if (ordered > available) {
+    
+//     return "Your order is too large, not enough goods in stock!";
+//   } 
+//     return "The order is accepted, our manager will contact you";
+  
+
+  
+//   // Change code above this line
+// }
+
+
+//xxxxxxxxxxxxxxxxxxxxxзадача № 4 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+
+// Change code below this line
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+//xxxxxxxxxxxxxxxxxxxxxзадача № 5 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// // Change code below this line
+// const firstElement = "apple";
+// const secondElement = "plum";
+// const lastElement = "orange";
+
+
+//xxxxxxxxxxxxxxxxxxxxxзадача № 6 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//

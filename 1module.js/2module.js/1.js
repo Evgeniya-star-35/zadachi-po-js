313d529e3ebcb7bdcaf89ba13331a99171a776b5
Evgeniya Bayda <baje75128@gmail.com>
2021-05-30 00:00:27 +0300

    14

--- 1module.js/2module.js/1.js
+++ 1module.js/2module.js/1.js
@@ -116,14 +116,81 @@
 
 //xxxxxxxxxxxxxxxxxxxxxзадача № 10 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
 
-function splitMessage(message, delimeter) {
-  let words;
-  // Change code below this line
-  console.log(delimeter.split(""));
-  console.log(message.split(" "));
-  // Change code above this line
-  return words;
-}
-console.log(splitMessage("Mango hurries to the train", " "));
-console.log(splitMessage("Mango", ""));
-console.log(splitMessage("best_for_week", "_")); 
+// function splitMessage(message, delimeter) {
+//   let words;
+//   // Change code below this line
+// words = message.split(delimeter);
+  
+//   // Change code above this line
+//   return words;
+// }
+// console.log(splitMessage("Mango hurries to the train", " "));
+// console.log(splitMessage("Mango", ""));
+// console.log(splitMessage("best_for_week", "_"));
+
+//xxxxxxxxxxxxxxxxxxxxxзадача № 11 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+
+// function calculateEngravingPrice(message, pricePerWord) {
+//    // Change code below this line
+// const words = message.split(' ');
+// const stringAll = words.length; 
+// const totalPrice = stringAll * pricePerWord;
+// return totalPrice;
+
+//    // Change code above this line
+// }
+// console.log(calculateEngravingPrice("JavaScript is in my blood", 10));
+// console.log(calculateEngravingPrice("JavaScript is in my blood", 20));
+// console.log(calculateEngravingPrice("Web-development is creative work", 40));
+// console.log(calculateEngravingPrice("Web-development is creative work", 20));
+
+//xxxxxxxxxxxxxxxxxxxxxзадача № 12 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+
+// function makeStringFromArray(array, delimeter) {
+//   let string;
+//   // Change code below this line
+   
+// string = array.join(delimeter);
+//   // Change code above this line
+//   return string;
+// }
+
+//xxxxxxxxxxxxxxxxxxxxxзадача № 13 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+
+// function slugify(title) {
+//   // Change code below this line
+
+
+// const slug = title.toLowerCase().split(' ').join('-');
+
+// return slug;
+//   // Change code above this line
+// }
+// console.log(slugify("Arrays for begginers"));
+// console.log(slugify("English for developer"));
+// console.log(slugify("Ten secrets of JavaScript"));
+// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+
+//xxxxxxxxxxxxxxxxxxxxxзадача № 14 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+
+// const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+// // Change code below this line
+// const firstTwoEls = fruits.slice(0, 2);
+// const nonExtremeEls = fruits.slice(1, 4);
+// const lastThreeEls = fruits.slice(-3);
+// console.log(firstTwoEls);
+// console.log(nonExtremeEls);
+// console.log(lastThreeEls);
+
+
+//xxxxxxxxxxxxxxxxxxxxxзадача № 15 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+
+const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+const newClients = ['Peach', 'Houston'];
+
+const allClients = oldClients.concat(newClients); // Change this line
+
+
+//xxxxxxxxxxxxxxxxxxxxxзадача № 16 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+

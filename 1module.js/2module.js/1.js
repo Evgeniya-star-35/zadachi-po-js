37ef570ce904e0d54bf3bb0ab2472063959437ce
Evgeniya Bayda <baje75128@gmail.com>
2021-05-30 22:40:41 +0300

    20

--- 1module.js/2module.js/1.js
+++ 1module.js/2module.js/1.js
@@ -302,3 +302,17 @@
 
 //xxxxxxxxxxxxxxxxxxxxxзадача № 20 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
 
+// Напиши функцию calculateTotalPrice(order), которая принимает один параметр order - массив чисел, и рассчитывает общую сумму его элементов. Общая сумма элементов должна сохраняться в переменной total, которая возвращается, как результат работы функции.
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+//   for (const value of order) {
+//     total += value;
+//   }
+//   // Change code above this line
+//   return total;
+// }
+
+//xxxxxxxxxxxxxxxxxxxxxзадача № 21 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+

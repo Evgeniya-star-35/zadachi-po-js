5082b404c67512b58b33bb0128561f77eed95929
Evgeniya Bayda <baje75128@gmail.com>
2021-05-31 14:57:30 +0300

    ''

--- 1module.js/2module.js/1.js
+++ 1module.js/2module.js/1.js
@@ -359,4 +359,38 @@
 
 //xxxxxxxxxxxxxxxxxxxxxзадача № 23 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
 
+// Напиши функцию filterArray(numbers, value), которая принимает массив чисел (параметр numbers) и возвращает новый массив, в котором будут только те элементы массива numbers, которые больше чем значение параметра value (число).
 
+// function filterArray(numbers, value) {
+//    // Change code below this line
+//  let newNumbers = [];
+// for (let i = 0; i > value; i += 1) {
+ 
+//   newNumbers = numbers.push(i);
+ 
+//   }
+
+//   return newNumbers;
+
+
+//   // Change code above this line
+// }
+
+
+function filterArray(numbers, value) {
+  // Change code below this line
+  let newNumbers = [];
+  for (const number of numbers) {
+    if (number > value) {
+ 
+      newNumbers = numbers.push(number);
+    }
+  }
+  return newNumbers;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
+console.log(filterArray());

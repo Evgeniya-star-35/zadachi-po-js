81161c665659e64d4515a0e6a5c9882850b2ac82
Evgeniya Bayda <baje75128@gmail.com>
2021-05-31 20:56:16 +0300

    32

--- 1module.js/2module.js/1.js
+++ 1module.js/2module.js/1.js
@@ -529,3 +529,22 @@
 
 //xxxxxxxxxxxxxxxxxxxxxзадача № 32 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
 
+// Напиши функцию includes(array, value), которая делает тоже самое, что и метод массива массив.includes(значение) - проверяет, есть ли в массиве array значение value, возвращая true если есть и false в противном случае.
+
+// При выполнении этой задачи в теле функции includes() нельзя использовать метод массив.includes(значение).
+
+// function includes(array, value) {
+//   // Change code below this line
+
+//     for (let i = 0; i < array.length; i += 1) {
+//         if (array[i] === value) {
+//             return true;
+//         }
+//     }
+  
+//     return false;
+
+//   // Change code above this line
+// }
+
+

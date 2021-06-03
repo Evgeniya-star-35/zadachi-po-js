21d5ac3632288ada42d031e33da60150f0b1dd11
Evgeniya Bayda <baje75128@gmail.com>
2021-06-03 10:57:42 +0300

    =

--- 1module.js/3module.js
+++ 1module.js/3module.js
@@ -547,3 +547,9 @@
 
 //xxxxxxxxxxxxxxxxxxxxxзадача № 12 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
 
+// Задача. Подсчёт свойств
+// Задание
+// Напиши функцию countProps(object), которая считает и возвращает количество собственных свойств объекта в параметре object. Используй переменную propCount для хранения количества свойств объекта.
+
+
+

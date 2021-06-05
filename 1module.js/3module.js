f730de79a22f8fdff2299d216a6ad2dbae4683cc
Evgeniya Bayda <baje75128@gmail.com>
2021-06-05 13:16:48 +0300

    task 29

--- 1module.js/3module.js
+++ 1module.js/3module.js
@@ -1268,4 +1268,21 @@
 
 // Для того чтобы получить финальные настройки теста, необходимо взять настройки по умолчанию и поверх них применить переопределённые настройки. Дополни код так, чтобы в переменной finalSettings получился объект финальных настроек теста.
 
+// const defaultSettings = {
+//   theme: 'light',
+//   public: true,
+//   withPassword: false,
+//   minNumberOfQuestions: 10,
+//   timePerQuestion: 60,
+// };
+// const overrideSettings = {
+//   public: false,
+//   withPassword: true,
+//   timePerQuestion: 30,
+// };
+// // Change code below this line
+// const finalSettings = { ...defaultSettings, ...overrideSettings };
+
+// / /xxxxxxxxxxxxxxxxxxxxxзадача № 30 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+
 

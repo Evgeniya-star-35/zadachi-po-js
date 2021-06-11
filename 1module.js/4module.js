0f8b1ba2ad40cf76426ca8a0f04784619556c961
Evgeniya Bayda <baje75128@gmail.com>
2021-06-11 22:16:01 +0300

    ++

--- 1module.js/4module.js
+++ 1module.js/4module.js
@@ -451,4 +451,86 @@
 // const burgerShackComposer = composeMessage.bind(burgerShack);
 // const burgerShackMessage = burgerShackComposer('Поли');
 
+//                  задача 10
+
+// Метод bind и методы объекта
+// При передаче методов объекта как колбэк-функций, контекст не сохраняется. Колбэк это ссылка на метод, которая присваивается как значение параметра, вызываемого без объекта.
+
+// const customer = {
+//   firstName: 'Jacob',
+//   lastName: 'Mercer',
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+
+// function makeMessage(callback) {
+//   // callback() это вызов метода getFullName без объекта
+//   console.log(`Обрабатываем заявку от ${callback()}.`);
+// }
+
+// makeMessage(customer.getFullName); // Будет ошибка при вызове функции
+// В строгом режиме, значение this в методе getFullName, при вызове как колбэк - функции callback(), будет undefined.При обращении к свойствам firstName и lastName будет
+// ошибка, так как undefined это не объект.
+
+// Метод bind используется для привязки контекста при передаче методов объекта как колбэк - функций.Передадим колбэком не оригинальный метод getFullName, а его копию
+// с привязанным контекстом к объекту customer.
+
+// // ❌ Было
+// makeMessage(customer.getFullName); // Будет ошибка при вызове функции
+
+// // ✅ Стало
+// makeMessage(customer.getFullName.bind(customer)); // Обрабатываем заявку от Jacob Mercer.
+// Задание
+// Сервису рассылки электронной почты необходимо добавить логирование действий для сбора статистики.Функция logAndInvokeAction(email, action) ожидает почту и
+// действие которое нужно выполнить - ссылку на метод объекта service.Сбор статистики симулируется логированием строки.Разберись и дополни код так, чтобы он работал верно.
+
+
+const service = {
+  mailingList: ['mango@mail.com', 'poly@hotmail.de', 'ajax@jmail.net'],
+  subscribe(email) {
+    service.mailingList.push(email);
+    return `Почта ${email} добавлена в рассылку.`;
+  },
+  unsubscribe(email) {
+    // console.log(service.unsubscribe);
+    service.mailingList = service.mailingList.filter((e) => e !== email);
+    return `Почта ${email} удалена из рассылки.`;
+  },
+};
+
+function logAndInvokeAction(email, action) {
+  console.log(`Выполняем действие с ${email}.`);
+  return `Выполняем действие с ${email}.`;
+}
+
+const firstInvoke = logAndInvokeAction('kiwi@mail.uk', service.subscribe);
+console.log(firstInvoke);
+// Почта kiwi@mail.uk добавлена в рассылку.
+console.log(logAndInvokeAction(service.subscribe.bind(service))); 
+
+// console.log(service.mailingList);
+/* ['mango@mail.com', 
+    'poly@hotmail.de', 
+    'ajax@jmail.net', 
+    'kiwi@mail.uk']*/
+const secondInvoke = logAndInvokeAction('poly@hotmail.de', service.unsubscribe);
+console.log(secondInvoke);
+// Почта poly@hotmail.de удалена из рассылки.
+
+// console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

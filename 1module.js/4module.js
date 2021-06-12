1a04b58ef0c0988a222a76ccffc24a0ce04e18ca
Evgeniya Bayda <baje75128@gmail.com>
2021-06-12 18:45:45 +0300

    10 task

--- 1module.js/4module.js
+++ 1module.js/4module.js
@@ -486,51 +486,35 @@
 // действие которое нужно выполнить - ссылку на метод объекта service.Сбор статистики симулируется логированием строки.Разберись и дополни код так, чтобы он работал верно.
 
 
-const service = {
-  mailingList: ['mango@mail.com', 'poly@hotmail.de', 'ajax@jmail.net'],
-  subscribe(email) {
-    service.mailingList.push(email);
-    return `Почта ${email} добавлена в рассылку.`;
-  },
-  unsubscribe(email) {
-    // console.log(service.unsubscribe);
-    service.mailingList = service.mailingList.filter((e) => e !== email);
-    return `Почта ${email} удалена из рассылки.`;
-  },
-};
+// const service = {
+//   mailingList: ['mango@mail.com', 'poly@hotmail.de', 'ajax@jmail.net'],
+//   subscribe(email) {
+//     this.mailingList.push(email);
+//     return `Почта ${email} добавлена в рассылку.`;
+//   },
+//   unsubscribe(email) {
+//     this.mailingList = this.mailingList.filter((e) => e !== email);
+//     return `Почта ${email} удалена из рассылки.`;
+//   },
+// };
 
-function logAndInvokeAction(email, action) {
-  console.log(`Выполняем действие с ${email}.`);
-  return `Выполняем действие с ${email}.`;
-}
+// function logAndInvokeAction(email, action) {
+//   console.log(`Выполняем действие с ${email}.`);
+//   return action(email);
+// }
 
-const firstInvoke = logAndInvokeAction('kiwi@mail.uk', service.subscribe);
-console.log(firstInvoke);
-// Почта kiwi@mail.uk добавлена в рассылку.
-console.log(logAndInvokeAction(service.subscribe.bind(service))); 
+// const firstInvoke = logAndInvokeAction('kiwi@mail.uk', service.subscribe.bind(service));
+// console.log(firstInvoke);
+// // Почта kiwi@mail.uk добавлена в рассылку.
 
 // console.log(service.mailingList);
-/* ['mango@mail.com', 
-    'poly@hotmail.de', 
-    'ajax@jmail.net', 
-    'kiwi@mail.uk']*/
-const secondInvoke = logAndInvokeAction('poly@hotmail.de', service.unsubscribe);
-console.log(secondInvoke);
-// Почта poly@hotmail.de удалена из рассылки.
+// /* ['mango@mail.com', 
+//     'poly@hotmail.de', 
+//     'ajax@jmail.net', 
+//     'kiwi@mail.uk']*/
+// const secondInvoke = logAndInvokeAction('poly@hotmail.de', service.unsubscribe.bind(service));
+// console.log(secondInvoke);
+// // Почта poly@hotmail.de удалена из рассылки.
 
 // console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 

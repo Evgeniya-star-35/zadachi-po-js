f3436b94077e922983fe92f190ab6ce2f7dfe3a8
Evgeniya Bayda <baje75128@gmail.com>
2021-06-14 01:20:53 +0300

    3 task

--- 1module.js/6module.js
+++ 1module.js/6module.js
@@ -77,3 +77,31 @@
 
 //                                                                  Задача №  3
 
+
+// Задача. Общие элементы
+// Задание
+// Функция getCommonElements(firstArray, secondArray) принимает два массива произвольной длины в параметры firstArray и secondArray, и возвращает новый массив их общих элементов,
+//     то есть тех которые есть в обоих массивах.
+
+// Выполни рефакторинг функции так, чтобы вместо цикла for она использовала метод forEach.
+
+//                                                                    ответ 
+
+// function getCommonElements(firstArray, secondArray) {
+//     const commonElements = [];
+//     // Пиши код ниже этой строки
+  
+//     firstArray.forEach(function(item) {
+//       if (secondArray.includes(item)) {
+//         commonElements.push(item);
+//       }
+//     })
+  
+//     return commonElements;
+//     // Пиши код выше этой строки
+//   }
+
+
+//                                                                  Задача №  4
+
+
